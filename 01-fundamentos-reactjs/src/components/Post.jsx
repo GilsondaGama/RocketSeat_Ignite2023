ac2dd1b02344a8aded2de32a079ce86f035556fa
Gilson da Gama <gilsondagama@hotmail.com>
2023-02-24 20:54:24 -0300

    Estrutura da aplicação - Componente: Post

---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,26 @@
+import styles from "./Post.module.css"
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/GilsondaGama.png" />
+          <div className={styles.authorinfo}>
+            <strong>Gilson da Gama</strong>
+            <span>Web Developre</span>
+          </div>
+        </div>
+
+        <time title="11 de maio às 08:13h" dateTime="2023-05-11">Publicado há 1 hora</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala pessoal 👋</p>
+        <p>Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻 </p>
+        <p>Acesse e deixe seu feedback 👉 devonlane.design</p>
+        <p><a href="">#uiux #userexperience</a></p>
+      </div>
+    </article>
+  )
+}
