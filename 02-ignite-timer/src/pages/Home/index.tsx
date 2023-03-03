47011a0d874be28114921cfc28ebc9faf0a715e1
Gilson da Gama <gilsondagama@hotmail.com>
2023-03-03 19:57:19 -0300

    Funcionalidades da aplicação - Iniciando novo ciclo

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Play } from 'phosphor-react'
 
 import { useForm } from 'react-hook-form'
@@ -21,7 +22,16 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [currentCycleId, setCurrentCycleId] = useState<string | null>(null)
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -30,10 +40,21 @@ export function Home() {
     },
   })
 
-  function handleCreateNewCycle(data: any) {
-    console.log(data)
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setCurrentCycleId(id)
     reset()
   }
+
+  const activeCyrcle = cycles.find((cycle) => cycle.id === currentCycleId)
 
   const task = watch('task')
   const isSubmitDisabled = !task
