e5fc20146b3a423f0837adc08b607cdf1ad29454
Gilson da Gama <gilsondagama@hotmail.com>
2023-08-01 15:50:09 -0300

    Páginas e rotas - Aprimorando os inputs

--- 02-ignite-timer-v2/src/pages/Home/index.tsx
+++ 02-ignite-timer-v2/src/pages/Home/index.tsx
@@ -17,11 +17,20 @@ export function Home() {
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
 
+          <datalist id="taskSuggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
+
           <label htmlFor="">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutos.</span>
         </FormContainer>
