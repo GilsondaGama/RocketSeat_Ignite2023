59f12503a2ab6cc57c2e14cc957dc84a64a009c0
Gilson da Gama <gilsondagama@hotmail.com>
2023-08-02 03:40:12 -0300

    Formulários - Validando formulários

--- 02-ignite-timer-v2/src/pages/Home/index.tsx
+++ 02-ignite-timer-v2/src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import { Play } from 'phosphor-react'
 import {
@@ -11,8 +13,18 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa.'),
+  minutesAmount: zod
+    .number()
+    .min(5, { message: 'O tempo mínimo é de 5 minutos' })
+    .max(60, { message: 'O tempo máximo é de 60 minutos' }),
+})
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
@@ -45,8 +57,8 @@ export function Home() {
             id="minutesAmount"
             placeholder="00"
             step={5}
-            min={5}
-            max={60}
+            // min={5}
+            // max={60}
             {...register('minutesAmount', { valueAsNumber: true })}
           />
           <span>minutos.</span>
