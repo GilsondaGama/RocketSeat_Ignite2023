83df87f828a00cc9247b2bb584fe51dd853b4eea
Gilson da Gama <gilsondagama@hotmail.com>
2023-08-02 17:02:10 -0300

    Funcionalidades da aplicação - Reduzindo countdown

--- 02-ignite-timer-v2/src/pages/Home/index.tsx
+++ 02-ignite-timer-v2/src/pages/Home/index.tsx
@@ -12,7 +12,8 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa.'),
@@ -28,6 +29,7 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startedAt?: Date
 }
 
 export function Home() {
@@ -43,6 +45,18 @@ export function Home() {
     },
   })
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startedAt!),
+        )
+      }, 1000)
+    }
+  }, [activeCycle])
+
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
 
@@ -50,14 +64,13 @@ export function Home() {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startedAt: new Date(),
     }
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     reset()
   }
-
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
