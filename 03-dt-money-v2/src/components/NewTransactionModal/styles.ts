bc4fdf24dc0145f53af99511d24d4aea062caf58
Gilson da Gama <gilsondagama@hotmail.com>
2023-08-08 16:19:44 -0300

    dt-money: Estrutura visual - Botões de entrada e saída

--- 03-dt-money-v2/src/components/NewTransactionModal/styles.ts
+++ 03-dt-money-v2/src/components/NewTransactionModal/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import * as Dialog from '@radix-ui/react-dialog'
-// import * as RadioGroup from '@radix-ui/react-radio-group'
+import * as RadioGroup from '@radix-ui/react-radio-group'
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -16,7 +16,6 @@ export const Content = styled(Dialog.Content)`
   padding: 2.5rem 3rem;
   background: ${(props) => props.theme['gray-800']};
 
-  // Hack para centralizar o modal na tela
   position: fixed;
   top: 50%;
   left: 50%;
@@ -76,50 +75,50 @@ export const CloseButton = styled(Dialog.Close)`
   color: ${(props) => props.theme['gray-500']};
 `
 
-// export const TransactionType = styled(RadioGroup.Root)`
-//   display: grid;
-//   grid-template-columns: repeat(2, 1fr);
-//   gap: 1rem;
-//   margin-top: 0.5rem;
-// `
+export const TransactionType = styled(RadioGroup.Root)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`
 // interface TransactionTypeButtonProps {
-// interface TransactionTBP {
-//   variant: 'income' | 'outcome'
-// }
+interface TransactionTBP {
+  variant: 'income' | 'outcome'
+}
 
-// export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTBP>`
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   gap: 0.5rem;
-//   border-radius: 6px;
-//   cursor: pointer;
-//   border: 0;
-//   padding: 1rem;
-//   background: ${(props) => props.theme['gray-700']};
-//   color: ${(props) => props.theme['gray-300']};
+export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTBP>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  padding: 1rem;
+  background: ${(props) => props.theme['gray-700']};
+  color: ${(props) => props.theme['gray-300']};
 
-//   svg {
-//     color: ${(props) =>
-//       props.variant === 'income'
-//         ? props.theme['green-300']
-//         : props.theme['red-300']};
-//   }
+  svg {
+    color: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-300']
+        : props.theme['red-300']};
+  }
 
-//   &[data-state='unchecked'] {
-//     background: ${(props) => props.theme['gray-300']};
-//     transition: background-color 0.2s;
-//   }
+  &[data-state='unchecked'] {
+    background: ${(props) => props.theme['gray-300']};
+    transition: background-color 0.2s;
+  }
 
-//   &[data-state='checked'] {
-//     color: ${(props) => props.theme.white};
-//     background: ${(props) =>
-//       props.variant === 'income'
-//         ? props.theme['green-500']
-//         : props.theme['red-500']};
+  &[data-state='checked'] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-500']
+        : props.theme['red-500']};
 
-//     svg {
-//       color: ${(props) => props.theme.white};
-//     }
-//   }
-// `
+    svg {
+      color: ${(props) => props.theme.white};
+    }
+  }
+`
