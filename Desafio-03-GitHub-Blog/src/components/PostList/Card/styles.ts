6342b6a3f3d61d49a85a71df27167c3b5f62b242
Gilson da Gama <gilsondagama@hotmail.com>
2023-08-14 18:50:18 -0300

    github blog: Estilização da lista de posts em Cards

--- Desafio-03-GitHub-Blog/src/components/PostList/Card/styles.ts
+++ Desafio-03-GitHub-Blog/src/components/PostList/Card/styles.ts
@@ -6,23 +6,38 @@ export const CardContent = styled.div`
   width: 26rem;
   height: 16.25rem;
   border-radius: 10px;
-  padding: 1.5rem;
+  padding: 2rem;
   background: ${(props) => props.theme['base-post']};
+
+  p {
+    display: -webkit-box;
+    -webkit-line-clamp: 4; /* Número de linhas que você deseja mostrar */
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    font-size: 1rem;
+    font-weight: 400;
+    margin-top: 1.5rem;
+    line-height: 1.6rem;
+  }
 `
 export const CardTitle = styled.div`
   display: flex;
+  justify-content: center;
 
   strong {
-    font-size: 1.5rem;
+    font-size: 1.3rem;
     font-weight: 400;
-    margin-bottom: 1rem;
+    line-height: 2rem;
     color: ${(props) => props.theme['base-title']};
   }
 
   span {
-    font-size: 1rem;
+    font-size: 14px;
     font-weight: 400;
     white-space: nowrap;
+    padding-left: 1rem;
     color: ${(props) => props.theme['base-span']};
   }
 `
