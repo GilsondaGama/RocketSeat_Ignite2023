1d9d59270320852a099fbc2655f2d703ffd7b57e
Gilson da Gama <gilsondagama@hotmail.com>
2023-08-27 07:22:03 -0300

    04-ignite-shop: Estrutura visual - Criando carrossel

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,16 +1,51 @@
 import { HomeContainer, Product } from '@/styles/pages/home'
 import Image from 'next/image'
 
+import { useKeenSlider } from 'keen-slider/react'
+
 import camisa1 from '../assets/camisas/1.png'
 import camisa2 from '../assets/camisas/2.png'
 import camisa3 from '../assets/camisas/3.png'
 import camisa4 from '../assets/camisas/4.png'
 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camisa1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camisa Branca</strong>
+          <span>R$ 129,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camisa Branca</strong>
+          <span>R$ 129,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camisa Branca</strong>
+          <span>R$ 129,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camisa4} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camisa Branca</strong>
--- 04-ignite-shop/src/styles/pages/home.ts
+++ 04-ignite-shop/src/styles/pages/home.ts
@@ -2,9 +2,8 @@ import { styled } from '..'
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
   width: '100%',
-  maxWidth: 'calc(100vm - (100vm - 1180px) / 2)',
+  maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   margin: '0 auto',
   minHeight: 656,
 })
@@ -12,10 +11,10 @@ export const HomeContainer = styled('main', {
 export const Product = styled('a', {
   background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
   borderRadius: 8,
-  padding: '0.25rem',
   cursor: 'pointer',
   position: 'relative',
   overflow: 'hidden',
+  minWidth: 540,
 
   display: 'flex',
   alignItems: 'center',
